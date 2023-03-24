7e582ef65763472bd42c60c9000ce4da20654c6f
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-03-24 14:04:02 -0400

    added the functionality to determine whether the profile page we are on belongs to the logged in user or not

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,20 +1,57 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '@/context/AuthContext';
+import { GetServerSideProps } from 'next';
 
-export default function ProfilePage() {
-    const { currentUser, githubData, currentUserData } = useAuth();
+interface ProfilePageProps {
+    profileName: string;
+    data: any;
+}
+
+export default function ProfilePage({ profileName, data }: ProfilePageProps) {
+    const { currentUser } = useAuth();
+    const [isCurrentUser, setIsCurrentUser] = useState<boolean>();
 
     useEffect(() => {
-        console.log('Current User: ', currentUser);
-        console.log('Github Data: ', githubData);
-        console.log('Current Database Data: ', currentUserData);
-    }, [currentUser, githubData, currentUserData]);
+        if (currentUser) {
+            if (profileName === currentUser?.displayName) {
+                setIsCurrentUser(true);
+            } else {
+                setIsCurrentUser(false);
+            }
+        }
+    }, [currentUser]);
 
-    // This page should get and display the data of the user whos username is in the url
     return (
         <div>
-            <h1>User Profile Page</h1>
-            <a href="/profile/settings">Settings</a>
+            {isCurrentUser === undefined ? (
+                <p>Loading</p>
+            ) : (
+                <>
+                    {isCurrentUser ? (
+                        <>
+                            <h1>This is the profile of the logged in user</h1>
+                            <p>{currentUser?.displayName}</p>
+                        </>
+                    ) : (
+                        <>
+                            <h1>This is the profile of a different user</h1>
+                            <p>{profileName}</p>
+                        </>
+                    )}
+                    <a href="/profile/settings">Settings</a>
+                </>
+            )}
         </div>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const params = context?.params;
+
+    return {
+        props: {
+            profileName: params?.username,
+            data: { name: 'Hello' },
+        },
+    };
+};
