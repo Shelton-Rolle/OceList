32743a2bf8c1fc43d9e7625ad79054de01396f5d
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-03-24 17:06:02 -0400

    started working on the project page for individual projects. The getServerSideProps function gets the project info from the api, all we need to do is use that id from that info to get the more detailed repository data from the github api

---
+++ src/database/GetProject.ts
@@ -0,0 +1,19 @@
+export default async function GetProject(
+    projectId: string
+): Promise<{ data: any; errors: string[] } | undefined> {
+    let result;
+
+    const data = {
+        apiKey: 'test123456',
+    };
+
+    await fetch(
+        `http://localhost:3001/projects/${projectId}?` +
+            new URLSearchParams(data)
+    )
+        .then((res) => res.json())
+        .then((res) => (result = res))
+        .catch((error) => console.log(error));
+
+    return result;
+}
---
+++ src/pages/projects/[projectId].tsx
@@ -0,0 +1,26 @@
+import GetProject from '@/database/GetProject';
+import { GetServerSideProps } from 'next';
+import { useEffect } from 'react';
+
+export default function ProjectPage({ projectId, project }: any) {
+    useEffect(() => {
+        console.log('Project Id: ', projectId);
+        console.log('Data: ', project);
+    }, []);
+
+    return <div>Project Page</div>;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const projectId = context?.params?.projectId;
+
+    const data = await GetProject(projectId as string);
+    const project = data?.data;
+
+    return {
+        props: {
+            projectId,
+            project,
+        },
+    };
+};
