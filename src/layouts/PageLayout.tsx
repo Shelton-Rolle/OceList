601f1931d019158d14605a6e4f046bade10212b3
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-03-26 15:01:06 -0400

    fixed the content so the menu does not go away on scroll

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -3,9 +3,12 @@ import { PageLayoutProps } from '@/types/props';
 
 export const PageLayout = ({ children }: PageLayoutProps) => {
     return (
-        <main className="grid grid-cols-12 pt-20 px-8 max-w-7xl mx-auto h-screen gap-10">
+        <main className="grid grid-cols-12 pt-20 px-8 mx-auto h-screen gap-10 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <PageHeader />
-            <div id="content" className="col-span-9">
+            <div
+                id="content"
+                className="col-span-9 overflow-y-scroll scrollbar-hide"
+            >
                 {children}
             </div>
         </main>
