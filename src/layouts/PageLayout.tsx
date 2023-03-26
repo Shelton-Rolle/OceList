9ffd334f73ef11f10710dee7a64e6ec347d543c9
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-03-26 14:57:31 -0400

    removed outline from main section

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -3,7 +3,7 @@ import { PageLayoutProps } from '@/types/props';
 
 export const PageLayout = ({ children }: PageLayoutProps) => {
     return (
-        <main className="outline outline-2 outline-black grid grid-cols-12 pt-20 px-8 max-w-7xl mx-auto h-screen gap-10">
+        <main className="grid grid-cols-12 pt-20 px-8 max-w-7xl mx-auto h-screen gap-10">
             <PageHeader />
             <div id="content" className="col-span-9">
                 {children}
