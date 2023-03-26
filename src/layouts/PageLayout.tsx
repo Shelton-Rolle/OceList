5755aba2b23fdfd94f17988dd305073a9cfaf006
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-03-26 15:04:22 -0400

    removed fixed styling on content, was causing other styling issues

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -3,11 +3,11 @@ import { PageLayoutProps } from '@/types/props';
 
 export const PageLayout = ({ children }: PageLayoutProps) => {
     return (
-        <main className="grid grid-cols-12 pt-20 px-8 mx-auto h-screen gap-10 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <main className="grid grid-cols-12 pt-20 px-8 max-w-7xl mx-auto h-screen gap-10">
             <PageHeader />
             <div
                 id="content"
-                className="col-span-9 overflow-y-scroll scrollbar-hide"
+                className="col-span-9 overflow-y-scroll scrollbar-hide px-7"
             >
                 {children}
             </div>
