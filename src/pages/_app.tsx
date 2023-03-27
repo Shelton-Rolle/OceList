807275fa01a93681018ea351a4f3e6faf516a74f
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-03-27 01:59:30 -0400

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/main.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '@/context/AuthContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
             <Component {...pageProps} />
+            <Analytics />
         </AuthProvider>
     );
 }
