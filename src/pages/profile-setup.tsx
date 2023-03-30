9d4826a036e9972917cf3dd491704f2ebee10a5b
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-03-30 18:40:49 -0400

    updated profile-setup to navigate to home page instead of profile page

--- src/pages/profile-setup.tsx
+++ src/pages/profile-setup.tsx
@@ -73,7 +73,7 @@ export default function ProfileSetup() {
                         ).then(async ({ result }) => {
                             setCurrentUserData(fullUser);
                             await sendEmailVerification(currentUser!);
-                            router.push(`/${githubData?.login}`);
+                            router.push(`/`);
                         });
                     });
                 });
