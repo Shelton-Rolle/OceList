48c0a3ad1727df0297f09e85509239aa029128c0
SheltonRolle_Mac <sheltonrolle7@gmail.com>
2023-04-04 21:46:55 -0400

    removed favicon from Heads of all pages

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -53,11 +53,6 @@ export default function Login() {
             <Head>
                 <title>Login</title>
                 <meta name="description" content="Login page" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
             </Head>
             <PageLayout>
                 <section className="flex justify-center items-center h-full">
